e8a9fcc5f0d9de2c057964a4885dba166ccf21ab
pankona <yosuke.akatsuka@gmail.com>
2021-04-30 16:10:31 +0900

    don't use import

--- src/line.ts
+++ src/line.ts
@@ -1,15 +1,12 @@
-import URLFetchRequestOptions = GoogleAppsScript.URL_Fetch.URLFetchRequestOptions;
-
 export const sendToLine = (mail: string) => {
   const LINE_NOTIFY_TOKEN = PropertiesService.getScriptProperties().getProperty(
     "LINE_NOTIFY_TOKEN"
   );
   const ENDPOINT = "https://notify-api.line.me/api/notify";
-  const options: URLFetchRequestOptions = {
+
+  UrlFetchApp.fetch(ENDPOINT, {
     method: "post",
     headers: { Authorization: `Bearer ${LINE_NOTIFY_TOKEN}` },
     payload: { message: mail },
-  };
-
-  UrlFetchApp.fetch(ENDPOINT, options);
+  });
 };
